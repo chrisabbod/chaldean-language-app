09e7b11f542ab05d94d688fcd5d4015bd0584fd0
Christopher Abbod <chrisabbod@gmail.com>
2023-06-15 19:55:32 -0400

    Create LessonScreen for Lesson Compose functions and display title of lesson

--- app/src/main/java/com/example/chaldean_language_app/MainActivity.kt
+++ app/src/main/java/com/example/chaldean_language_app/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.chaldean_language_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import com.example.chaldean_language_app.Utils.loadJSONFromAsset
+import com.example.chaldean_language_app.Utils.parseJSON
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val jsonString = loadJSONFromAsset(this, "language_lessons.json")
+        val lessonData = parseJSON(jsonString)
+
+        val firstLessonTitle = lessonData.lessons[0].lesson_title
+
+        setContent {
+            LessonTitle(title = firstLessonTitle)
+        }
     }
 }
