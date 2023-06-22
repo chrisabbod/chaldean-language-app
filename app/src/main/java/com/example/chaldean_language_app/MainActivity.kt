4d33326ec8e7bc09f1074aac215168e087611652
Christopher Abbod <chrisabbod@gmail.com>
2023-06-22 18:12:13 -0400

    Remove unused import statement

--- app/src/main/java/com/example/chaldean_language_app/MainActivity.kt
+++ app/src/main/java/com/example/chaldean_language_app/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.navigation.compose.rememberNavController
 import com.example.chaldean_language_app.data.utils.Utils.loadJSONFromAsset
 import com.example.chaldean_language_app.data.utils.Utils.parseJSON
 import com.example.chaldean_language_app.navigation.LessonNavGraph
-import com.example.chaldean_language_app.ui.screens.LessonListScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
